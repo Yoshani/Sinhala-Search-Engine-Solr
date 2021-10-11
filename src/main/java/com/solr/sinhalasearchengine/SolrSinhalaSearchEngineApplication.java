7a85ff97e9086b2d58efa4d70d0b4991e9438610
Yoshani <yoshani.malinka@gmail.com>
2021-10-11 22:50:42 +0530

    controller, entity, configs

--- src/main/java/com/solr/sinhalasearchengine/SolrSinhalaSearchEngineApplication.java
+++ src/main/java/com/solr/sinhalasearchengine/SolrSinhalaSearchEngineApplication.java
@@ -1,10 +1,30 @@
 package com.solr.sinhalasearchengine;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.solr.sinhalasearchengine.config.Configuration;
+import com.solr.sinhalasearchengine.controller.SearchController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.io.IOException;
+import org.apache.log4j.Logger;
+
 @SpringBootApplication
 public class SolrSinhalaSearchEngineApplication {
+
+	private static ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+	private static Logger logger = Logger.getLogger( SearchController.class);
+	public static Configuration configuration = null;
+
+	static {
+		try {
+			configuration = mapper.readValue(new File("config.yaml"), Configuration.class);
+		} catch ( IOException e) {
+			logger.error(e.getMessage());
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SolrSinhalaSearchEngineApplication.class, args);
