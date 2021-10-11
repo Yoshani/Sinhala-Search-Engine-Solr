c498a64e85dce70c077a0bd0192bd094ec3de431
Yoshani <yoshani.malinka@gmail.com>
2021-10-11 20:18:21 +0530

    first commit

---
+++ src/main/java/com/solr/sinhalasearchengine/SolrSinhalaSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.solr.sinhalasearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolrSinhalaSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolrSinhalaSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solr/sinhalasearchengine/SolrSinhalaSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solr.sinhalasearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolrSinhalaSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
